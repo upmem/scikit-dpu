e2f649468438de8a5557fd59c1c6ad22e94f7db6
SylvanBrocard <sylvan.brocard@gmail.com>
2022-02-26 21:27:45 +0000

    lightened the tests workload

--- skdpu/tree/tests/test_consistency.py
+++ skdpu/tree/tests/test_consistency.py
@@ -1,33 +1,32 @@
 # -*- coding: utf-8 -*-
 from skdpu.tree import DecisionTreeClassifierDpu
 from sklearn.tree import export_text
-from sklearn.datasets import make_classification
+from . import dataset_generator
+from .dataset_generator import get_classification_problem
+
+n_dpu = dataset_generator.n_dpu
 
 
 def test_consistency_new_classifier():
-    n_dpu = 50
-    n_points_per_dpu = 2000
-    n_features = 16
+    X, y = get_classification_problem()
 
-    X, y = make_classification(n_samples=n_dpu * n_points_per_dpu,
-                               n_features=n_features,
-                               n_informative=4,
-                               n_classes=3,
-                               random_state=0)
-
-    clf1 = DecisionTreeClassifierDpu(random_state=1,
-                                     criterion='gini_dpu',
-                                     splitter='random_dpu',
-                                     ndpu=n_dpu,
-                                     max_depth=10)
+    clf1 = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf1.fit(X, y)
 
-    clf2 = DecisionTreeClassifierDpu(random_state=1,
-                                     criterion='gini_dpu',
-                                     splitter='random_dpu',
-                                     ndpu=n_dpu,
-                                     max_depth=10)
+    clf2 = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf2.fit(X, y)
 
@@ -38,21 +37,15 @@ def test_consistency_new_classifier():
 
 
 def test_consistency_same_classifier():
-    n_dpu = 50
-    n_points_per_dpu = 2000
-    n_features = 16
+    X, y = get_classification_problem()
 
-    X, y = make_classification(n_samples=n_dpu * n_points_per_dpu,
-                                n_features=n_features,
-                                n_informative=4,
-                                n_classes=3,
-                                random_state=0)
-
-    clf1 = DecisionTreeClassifierDpu(random_state=1,
-                                        criterion='gini_dpu',
-                                        splitter='random_dpu',
-                                        ndpu=n_dpu,
-                                        max_depth=10)
+    clf1 = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf1.fit(X, y)
     r1 = export_text(clf1)
@@ -64,35 +57,27 @@ def test_consistency_same_classifier():
 
 
 def test_consistency_new_array():
-    n_dpu = 50
-    n_points_per_dpu = 2000
-    n_features = 16
+    X1, y1 = get_classification_problem()
 
-    X1, y1 = make_classification(n_samples=n_dpu * n_points_per_dpu,
-                               n_features=n_features,
-                               n_informative=4,
-                               n_classes=3,
-                               random_state=0)
-
-    clf1 = DecisionTreeClassifierDpu(random_state=1,
-                                     criterion='gini_dpu',
-                                     splitter='random_dpu',
-                                     ndpu=n_dpu,
-                                     max_depth=10)
+    clf1 = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf1.fit(X1, y1)
 
-    X2, y2 = make_classification(n_samples=n_dpu * n_points_per_dpu,
-                               n_features=n_features,
-                               n_informative=4,
-                               n_classes=3,
-                               random_state=0)
+    X2, y2 = get_classification_problem()
 
-    clf2 = DecisionTreeClassifierDpu(random_state=1,
-                                     criterion='gini_dpu',
-                                     splitter='random_dpu',
-                                     ndpu=n_dpu,
-                                     max_depth=10)
+    clf2 = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf2.fit(X2, y2)
 
--- skdpu/tree/tests/test_validity.py
+++ skdpu/tree/tests/test_validity.py
@@ -3,35 +3,32 @@ from skdpu.tree import DecisionTreeClassifierDpu
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
+from . import dataset_generator
+from .dataset_generator import get_classification_problem
+
+n_dpu = dataset_generator.n_dpu
 
 
 def test_validity():
-    n_dpu = 50
-    n_points_per_dpu = 2000
-    n_features = 16
-
     acceptable_delta = 0.1
 
-    X, y = make_classification(n_samples=n_dpu * n_points_per_dpu,
-                               n_features=n_features,
-                               n_informative=4,
-                               n_classes=3,
-                               random_state=0)
+    X, y = get_classification_problem()
 
-    clf_dpu = DecisionTreeClassifierDpu(random_state=1,
-                                        criterion='gini_dpu',
-                                        splitter='random_dpu',
-                                        ndpu=n_dpu,
-                                        max_depth=10)
+    clf_dpu = DecisionTreeClassifierDpu(
+        random_state=1,
+        criterion="gini_dpu",
+        splitter="random_dpu",
+        ndpu=n_dpu,
+        max_depth=10,
+    )
 
     clf_dpu.fit(X, y)
     y_pred_dpu = clf_dpu.predict(X)
     accuracy_gpu = accuracy_score(y, y_pred_dpu)
 
-    clf_cpu = DecisionTreeClassifier(random_state=1,
-                                     criterion='gini',
-                                     splitter='random',
-                                     max_depth=10)
+    clf_cpu = DecisionTreeClassifier(
+        random_state=1, criterion="gini", splitter="random", max_depth=10
+    )
 
     clf_cpu.fit(X, y)
     y_pred_cpu = clf_cpu.predict(X)
