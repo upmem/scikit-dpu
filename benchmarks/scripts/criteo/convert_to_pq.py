ada383244c2e1c6f57a6e3f6b2fc2d2803c0e545
SylvanBrocard <sylvan.brocard@gmail.com>
2022-11-15 14:57:52 +0100

    using dask for parquet conversion

--- benchmarks/scripts/criteo/convert_to_pq.py
+++ benchmarks/scripts/criteo/convert_to_pq.py
@@ -3,11 +3,24 @@ Converts the Criteo dataset CSV to a parquet file for faster access.
 """
 
 import pandas as pd
+import dask.dataframe as dd
 import numpy as np
+from dask.diagnostics import ProgressBar
 
-for i in range(7, 14):
-    filename = f"/home/upmemstaff/sbrocard/datasets/criteo/day_{i}.gz"
-    df = pd.read_csv(filename, dtype=np.float32, header=None, sep="\t", usecols=range(14), compression='gzip')
+typedict = dict(
+    [[i, np.float32] for i in range(14)] + [[i, str] for i in range(14, 40)]
+)
+# values = dict([str(i), 0] for i in range(14))
+
+for i in range(4):
+    filename = f"/scratch/sbrocard/datasets/criteo/day_{i}"
+    print(f"Processing {filename}...")
+    df = dd.read_csv(filename, dtype=typedict, header=None, sep="\t")
     df.columns = df.columns.astype(str)
-    df.fillna(0, inplace=True)
-    df.to_parquet(f"./data/day_{i}.pq", index=False, compression=None)
+    # df.fillna(value=values, inplace=True, axis=0)
+    with ProgressBar():
+        df.to_parquet(
+            f"/scratch/sbrocard/datasets/criteo_serialized/day_{i}.pq",
+            write_index=False,
+            compression=None,
+        )
