c7e8079a6acaeff260bacf929f4ab622801224be
SylvanBrocard <sylvan.brocard@gmail.com>
2022-11-28 16:22:53 +0100

    spark encoding

--- benchmarks/scripts/criteo/encoding_spark.py
+++ benchmarks/scripts/criteo/encoding_spark.py
@@ -9,9 +9,10 @@ from functools import reduce
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame, SparkSession
 
-NR_DAYS_IN_TRAIN_SET = 2
-NR_DAYS_IN_TEST_SET = NR_DAYS_IN_TRAIN_SET
+NR_DAYS_IN_TRAIN_SET = 1
+NR_DAYS_IN_TEST_SET = 0
 
+TARGET_COL = "0"
 
 def union_all(*dfs: DataFrame) -> DataFrame:
     return reduce(DataFrame.union, dfs)
@@ -19,9 +20,10 @@ def union_all(*dfs: DataFrame) -> DataFrame:
 
 def configure_spark() -> SparkSession:
     spark = (
-        SparkSession.builder.config("spark.driver.host", "127.0.0.1")
-        .config("spark.executor.memory", "10g")
-        .config("spark.driver.memory", "250g")
+        SparkSession.builder
+        .config("spark.driver.host", "127.0.0.1")
+        # .config("spark.executor.memory", "10g")
+        .config("spark.driver.memory", "256g")
         .config("spark.local.dir", "/scratch/sbrocard/sparklocaldir")
         # .config("spark.driver.maxResultSize", "150g")
         .appName("Criteo")
@@ -48,19 +50,19 @@ def target_mean_encoding(
         dataframes with target encoded columns
     """
     # target_encoded_columns_list = []
-    df_train_enriched = df_train
-    df_test_enriched = df_test
+    df_train_encoded = df_train
+    df_test_encoded = df_test
     for c in col:
         print(f"column: {c}")
         means = df_train.groupby(F.col(c)).agg(
-            F.mean(target).alias(f"{c}_mean_encoding")
+            F.mean(target).astype('float').alias(f"{c}_mean_encoding")
         )
-        df_train_enriched = df_train_enriched.join(means, on=c, how="left").drop(c)
-        df_test_enriched = df_test_enriched.join(means, on=c, how="left").drop(c)
-        # target_encoded_columns_list.append(f"{c}_mean_encoding")
+        df_train_encoded = df_train_encoded.join(means, on=c, how="left").drop(c)
+        if df_test is not None:
+            df_test_encoded = df_test_encoded.join(means, on=c, how="left").drop(c)
     return (
-        df_train_enriched,
-        df_test_enriched,
+        df_train_encoded,
+        df_test_encoded,
     )
 
 
@@ -86,46 +88,48 @@ if __name__ == "__main__":
     df_train = df_train.fillna(0)
     df_train = df_train.fillna("")
 
-    global_mean = df_train.select(F.mean("0")).collect()[0][0]
+    global_mean = df_train.select(F.mean(TARGET_COL)).collect()[0][0]
 
     print(f"got global mean: {global_mean}")
 
-    # del df_encoded
-    # del df
+    if NR_DAYS_IN_TEST_SET > 0:
+        print("reading test days")
 
-    print("reading test days")
+        df_test = union_all(
+            *[
+                spark.read.parquet(os.path.join(criteo_folder, f"day_{i}.pq"))
+                for i in range(
+                    NR_DAYS_IN_TRAIN_SET, NR_DAYS_IN_TRAIN_SET + NR_DAYS_IN_TEST_SET
+                )
+            ]
+        )
 
-    df_test = union_all(
-        *[
-            spark.read.parquet(os.path.join(criteo_folder, f"day_{i}.pq"))
-            for i in range(
-                NR_DAYS_IN_TRAIN_SET, NR_DAYS_IN_TRAIN_SET + NR_DAYS_IN_TEST_SET
-            )
-        ]
-    )
-
-    print("read")
-    df_test = df_test.fillna(0)
-    df_train = df_train.fillna("")
+        print("read")
+        df_test = df_test.fillna(0)
+        df_train = df_train.fillna("")
+    else:
+        df_test = None
 
     df_train_encoded, df_test_encoded = target_mean_encoding(
-        df_train, df_test, [str(i) for i in range(14, 40)], "0"
+        df_train, df_test, [str(i) for i in range(14, 40)], TARGET_COL
     )
 
-    # filling with the global target mean in case some hashes in the test set are not in the train set
-    df_test_encoded = df_test_encoded.fillna(global_mean)
-
-    df_train_encoded.write.parquet(
+    df_train_encoded.repartition(200).write.parquet(
         "/scratch/sbrocard/datasets/criteo_spark/"
         + f"train_day_0_to_{NR_DAYS_IN_TRAIN_SET-1}.pq",
         mode="overwrite",
-        compression='none',
+        compression="none",
     )
 
-    df_test_encoded.write.parquet(
-        "/scratch/sbrocard/datasets/criteo_spark/"
-        + f"test_day_{NR_DAYS_IN_TRAIN_SET}"
-        + f"_to_{NR_DAYS_IN_TRAIN_SET + NR_DAYS_IN_TEST_SET - 1}.pq",
-        mode="overwrite",
-        compression='none',
-    )
+    if df_test is not None:
+        # filling with the global target mean in case some hashes in the test set
+        # are not in the train set
+        df_test_encoded = df_test_encoded.fillna(global_mean)
+
+        df_test_encoded.repartition(200).write.parquet(
+            "/scratch/sbrocard/datasets/criteo_spark/"
+            + f"test_day_{NR_DAYS_IN_TRAIN_SET}"
+            + f"_to_{NR_DAYS_IN_TRAIN_SET + NR_DAYS_IN_TEST_SET - 1}.pq",
+            mode="overwrite",
+            compression="none",
+        )
